e0b574b17dfecde9830319108a0a860c3e9d2f2f
Shaik Mohammad Abdullah <smabdullahblr@gmail.com>
2024-06-30 00:03:41 +0530

    fix: controlled component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
+import React, { useState } from "react";
 import data from "./data/category-relation.json";
-import categories from "./data/id-to-name-map.json";
 import DropdownCheckbox from "./components/DropdownCheckbox";
 import "./App.css";
 
 function App() {
+  const [checkedItems, setCheckedItems] = useState({});
+  const [text, setText] = useState("");
+  const updateChildCheckboxes = (
+    id,
+    isChecked,
+    data,
+    updatedCheckedItems = {}
+  ) => {
+    if (data[id] && data[id]?.children) {
+      Object.keys(data[id].children).forEach((childId) => {
+        updatedCheckedItems[childId] = isChecked;
+
+        updateChildCheckboxes(
+          childId,
+          isChecked,
+          data[id].children,
+          updatedCheckedItems
+        );
+      });
+    }
+  };
+
+  const handleCheckboxChange = (e, categoryId, categoryValue, data) => {
+    const isChecked = e.target.checked;
+    const updatedCheckedItems = { ...checkedItems, [categoryId]: isChecked };
+    if (data[categoryId]) {
+      updateChildCheckboxes(categoryId, isChecked, data, updatedCheckedItems);
+    }
+    setCheckedItems(updatedCheckedItems);
+    setText((prev) => {
+      const items = prev ? prev.split(", ") : [];
+      const updatedItems = new Set(items);
+      if (isChecked) {
+        updatedItems.add(categoryValue);
+        if (data[categoryId] && data[categoryId].children) {
+          Object.keys(data[categoryId].children).forEach((childcategoryId) => {
+            updatedItems.add(
+              data[categoryId].children[childcategoryId].categoryValue
+            );
+          });
+        }
+      } else {
+        updatedItems.delete(categoryValue);
+        if (data[categoryId] && data[categoryId].children) {
+          Object.keys(data[categoryId].children).forEach((childcategoryId) => {
+            updatedItems.delete(
+              data[categoryId].children[childcategoryId].categoryValue
+            );
+          });
+        }
+      }
+      return Array.from(updatedItems).join(", ");
+    });
+  };
   return (
     <div className="App">
-      <DropdownCheckbox data={data.relationships} categories={categories} />
+      <DropdownCheckbox
+        data={data.relationships}
+        onChange={handleCheckboxChange}
+        checkedItems={checkedItems}
+        text={text}
+      />
     </div>
   );
 }
--- src/components/DropdownCheckbox/index.jsx
+++ src/components/DropdownCheckbox/index.jsx
@@ -2,13 +2,9 @@ import React, { useState, useEffect, useRef } from "react";
 import Dropdown from "../../images/Dropdown";
 import Tick from "../../images/Tick";
 
-function DropdownCheckbox({ data }) {
+function DropdownCheckbox({ data, handleCheckboxChange, text, checkedItems }) {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
-  const [checkedItems, setCheckedItems] = useState({});
-  const [text, setText] = useState("");
-
-  console.log(checkedItems);
 
   useEffect(() => {
     const handleOutsideClick = (event) => {
@@ -23,59 +19,6 @@ function DropdownCheckbox({ data }) {
     };
   }, []);
 
-  const updateChildCheckboxes = (
-    id,
-    isChecked,
-    data,
-    updatedCheckedItems = {}
-  ) => {
-    if (data[id] && data[id]?.children) {
-      Object.keys(data[id].children).forEach((childId) => {
-        updatedCheckedItems[childId] = isChecked;
-
-        updateChildCheckboxes(
-          childId,
-          isChecked,
-          data[id].children,
-          updatedCheckedItems
-        );
-      });
-    }
-  };
-
-  const handleCheckboxChange = (e, categoryId, categoryValue, data) => {
-    const isChecked = e.target.checked;
-    const updatedCheckedItems = { ...checkedItems, [categoryId]: isChecked };
-    if (data[categoryId]) {
-      updateChildCheckboxes(categoryId, isChecked, data, updatedCheckedItems);
-    }
-    setCheckedItems(updatedCheckedItems);
-    setText((prev) => {
-      const items = prev ? prev.split(", ") : [];
-      const updatedItems = new Set(items);
-      if (isChecked) {
-        updatedItems.add(categoryValue);
-        if (data[categoryId] && data[categoryId].children) {
-          Object.keys(data[categoryId].children).forEach((childcategoryId) => {
-            updatedItems.add(
-              data[categoryId].children[childcategoryId].categoryValue
-            );
-          });
-        }
-      } else {
-        updatedItems.delete(categoryValue);
-        if (data[categoryId] && data[categoryId].children) {
-          Object.keys(data[categoryId].children).forEach((childcategoryId) => {
-            updatedItems.delete(
-              data[categoryId].children[childcategoryId].categoryValue
-            );
-          });
-        }
-      }
-      return Array.from(updatedItems).join(", ");
-    });
-  };
-
   const renderCheckboxes = (data) => {
     return Object.keys(data).map((key) => {
       const { categoryId, children, categoryValue } = data[key] || {};
@@ -83,8 +26,6 @@ function DropdownCheckbox({ data }) {
       const isPartiallyChecked = Object.keys(children || {}).some(
         (childKey) => checkedItems[childKey]
       );
-
-      console.log(isPartiallyChecked);
 
       return (
         !categoryValue.includes("_diffnode") && (
